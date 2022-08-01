c47048e48c45e38076b16dbda671fd93d36f77e3
WilfriedM95 <wilfriedmoussong@gmail.com>
2022-08-01 16:35:10 +0200

    initial commit

---
+++ src/main/java/com/aizoon/Prodotto4Application.java
@@ -0,0 +1,13 @@
+package com.aizoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prodotto4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prodotto4Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/aizoon/security/ApplicationSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.aizoon.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder=passwordEncoder;
+	}
+	@Override
+	protected void configure(HttpSecurity http)throws Exception{
+		http.authorizeRequests()
+		.antMatchers("/","index","/css/*")
+		.permitAll()
+        .anyRequest().
+          authenticated().
+           and().
+            httpBasic();	
+		}
+	
+	/**creation d'utilisateur avec password
+	 * nom
+	 * password doit etre coder sinon il y'aura erreur
+	 * role
+	 * ..*/
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+	
+		UserDetails annaSmithUser=
+				User.builder()
+        .username("annasmith")
+        .password(passwordEncoder.encode("password"))
+        .roles("STUDENT")// ROLE_STUDENT
+        .build();
+		return new InMemoryUserDetailsManager(annaSmithUser);
+		
+	}
+	
+	
+}
